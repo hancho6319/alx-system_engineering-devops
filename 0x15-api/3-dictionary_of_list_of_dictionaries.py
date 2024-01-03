b1e82b9261fe3aa205296c9c293a2ec54196f5c3
hancho6319 <abbaauwalharuna@gmail.com>
2024-01-03 03:41:43 +0000

    GitHub repository: alx-system_engineering-devops     Directory: 0x15-api     File: 1-export_to_CSV.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """
-Script that retrieves data from a REST API 
-for employee TODO lists
-and exports the information in JSON format.
+Script that, using this REST API, for a given employee ID, returns
+information about his/her TODO list progress
+and export data in the JSON format.
 """
 
 import json
 import requests
 from sys import argv
 
-def fetch_data(url):
-    try:
-        response = requests.get(url)
-        response.raise_for_status()
-        return response.json()
-    except requests.RequestException as e:
-        print(f"Error fetching data from {url}: {e}")
-        return None
+if __name__ == "__main__":
+    api_users = requests.get("https://jsonplaceholder.typicode.com/users").json()
+    api_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
+    
+    all_employee_tasks = {}
 
-def generate_employee_tasks(users_data, todos_data):
-    employee_tasks = {}
-
-    for user in users_data:
+    for user in api_users:
         user_tasks = []
-        for task in todos_data:
+        for task in api_todos:
             if task.get('userId') == user.get('id'):
                 task_info = {
                     "username": user.get('username'),
@@ -31,21 +25,7 @@ def generate_employee_tasks(users_data, todos_data):
                     "completed": task.get('completed')
                 }
                 user_tasks.append(task_info)
-        employee_tasks[user.get('id')] = user_tasks
+        all_employee_tasks[user.get('id')] = user_tasks
 
-    return employee_tasks
-
-if __name__ == "__main__":
-    users_url = "https://jsonplaceholder.typicode.com/users"
-    todos_url = "https://jsonplaceholder.typicode.com/todos"
-
-    users_data = fetch_data(users_url)
-    todos_data = fetch_data(todos_url)
-
-    if users_data and todos_data:
-        all_employee_tasks = generate_employee_tasks(users_data, todos_data)
-
-        with open('all_employees_tasks.json', mode='w') as json_file:
-            json.dump(all_employee_tasks, json_file)
-            print("Data successfully exported to 'all_employees_tasks.json'")
-    else:
+    with open('all_employees_tasks.json', mode='w') as json_file:
+        json.dump(all_employee_tasks, json_file)
