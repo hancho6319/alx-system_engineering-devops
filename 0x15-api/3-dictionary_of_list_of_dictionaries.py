26eefd861017320d52ed541003304eb1a10194dc
hancho6319 <abbaauwalharuna@gmail.com>
2024-01-03 03:46:50 +0000

    GitHub repository: alx-system_engineering-devops     Directory: 0x15-api     File: 1-export_to_CSV.py

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,23 +9,28 @@ import json
 import requests
 from sys import argv
 
+
 if __name__ == "__main__":
-    api_users = requests.get("https://jsonplaceholder.typicode.com/users").json()
-    api_todos = requests.get('https://jsonplaceholder.typicode.com/todos').json()
-    
-    all_employee_tasks = {}
 
-    for user in api_users:
-        user_tasks = []
-        for task in api_todos:
+    import json
+    import requests
+    import sys
+
+    tds = requests.get('https://jsonplaceholder.typicode.com/todos')
+    usr = requests.get("https://jsonplaceholder.typicode.com/users")
+    tds = tds.json()
+    usr = usr.json()
+    todoAll = {}
+
+    for user in usr:
+        taskList = []
+        for task in tds:
             if task.get('userId') == user.get('id'):
-                task_info = {
-                    "username": user.get('username'),
-                    "task": task.get('title'),
-                    "completed": task.get('completed')
-                }
-                user_tasks.append(task_info)
-        all_employee_tasks[user.get('id')] = user_tasks
+                taskDict = {"username": user.get('username'),
+                            "task": task.get('title'),
+                            "completed": task.get('completed')}
+                taskList.append(taskDict)
+        todoAll[user.get('id')] = taskList
 
-    with open('all_employees_tasks.json', mode='w') as json_file:
-        json.dump(all_employee_tasks, json_file)
+    with open('todo_all_employees.json', mode='w') as f:
+        json.dump(todoAll, f)
