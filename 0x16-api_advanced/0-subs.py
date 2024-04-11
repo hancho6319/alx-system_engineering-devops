56eab8c068f55594089c463d3e6a3152cfd0dc70
hancho6319 <abbaauwalharuna@gmail.com>
2024-04-11 15:10:38 +0000

    GitHub repository: alx-system_engineering-devops Directory: 0x16-api_advanced File: 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Function to query subscribers on a given Reddit subreddit... """
+import requests
+
+def numberOfSubscribers(subreddit):
+    """ Return the total number of subscibers on a given subreddit... """
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
+    }
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 404:
+        return 0
+    result = response.json().get("data")
+    return result.get("subscribers")
+
---
+++ 0x16-api_advanced/README.md
@@ -0,0 +1,1 @@
+This is th 0x16. API advanced
